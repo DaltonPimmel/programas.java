5bf526a1f766f600ea50b5708fdae336617bebb8
Dalton <dalton.cco1@gmail.com>
2015-02-27 15:54:52 -0300

    teste em java

---
+++ exemplo/src/exemplo/Exemplo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exemplo;
+
+
+public class Exemplo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i = 10;
+        System.out.println(+ i);
+    }
+    
+}

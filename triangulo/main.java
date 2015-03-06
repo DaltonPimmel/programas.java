d25a3fbdd0abace2ab5325aad66c39b1f423fcf5
Dalton <dalton.cco1@gmail.com>
2015-03-06 16:43:10 -0300

    Calculoa da area e perimetro de um triangulo.

--- triangulo/main.java
+++ triangulo/main.java
@@ -1,17 +1,23 @@
-import java.util.Scanner;
+import java.util.Scanner; // receber entrada de dados do teclado
 
 class main{
 	
-
-	
-	public static void main(String[] args){
-		raio a = new raio();
-		double b;
-		Scanner in = new Scanner(System.in);
+	public static void main(String[] args){ // metodo principal
+		raio a = new raio();	// declarando um objeto do tipo raio
+		Scanner ent = new Scanner(System.in); // declarando ent do tipo Scanner
+		
 		System.out.println("Informe o comprimento do triangulo: ");
-		a.compri = 20;
-		a.altura = 2;
-		b = a.calcula(20, 2);
-		System.out.println(b);
+		a.compri = ent.nextDouble();
+		System.out.println("Informe a altura do triangulo: ");
+		a.altura = ent.nextDouble();
+		a.Raio(a.compri, a.altura);
+		
+		
+		
+		
+		
+	//	System.out.println("Raio do tringulo: " + a.Raio(a.compri, a.altura));
+	//	System.out.println("Perimetro do tringulo: " + a.Perimetro(a.compri, a.altura));
+		
 	}
 }
--- triangulo/raio.java
+++ triangulo/raio.java
@@ -1,13 +1,18 @@
 class raio{
 		public double altura;
-		public double compri;
+		public double compri;	
 	
-		static double calcula(double area, double altura){
-			double a;
-			a = (area * altura) / 2;
-			return a;
-	
+		static void Raio(double area, double altura){   // metodo para calcular o raio e o perimetro
+			double r, p;
+			r = (area * altura) / 2;
+			p = 2 * (area * altura);
+			imprime(r, p); // chama o metodo imprimir
+			
+		}	
+		static void imprime(double r, double p){     // metodo para imprimir o raio e o perimetro
+			System.out.println("Raio: " + r + "\nPerimetro: " + p);
 		}
-	
+		
 }
+		
 

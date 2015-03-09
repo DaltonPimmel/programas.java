79e8e7e3e9b9b1d0ce53eb6e5f7244ff9dbe070d
Dalton <dalton.cco1@gmail.com>
2015-03-09 15:44:30 -0300

    Testes de metodos e classes.

---
+++ Data/Data.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+class Data{
+	
+	private int dia;   // atributos da classe
+	private int mes;
+	private int ano;
+	
+	public Data(){ // recebe valores por padrão.
+		this.dia = 1;
+		this.mes = 1;
+		this.ano = 1970;	
+	}
+
+	public void RecebeDados(){   // metodo para receber o dia mes e ano.
+		Scanner input = new Scanner(System.in);
+		System.out.println("Informe o dia: ");
+		int d = input.nextInt();
+		System.out.println("Informe o mes: ");
+		int m = input.nextInt();
+		System.out.println("Informe o ano: ");
+		int a = input.nextInt();
+		
+		if(m != 2 && d > 0 && d < 31 ){
+			this.dia = d;
+			this.mes = m;
+			this.ano = a;
+		}
+	}
+	
+	public void imprime(){  // metodo para imprimir os dados.
+		System.out.println(this.dia + "/" + this.mes + "/" + this.ano);
+	}
+}
---
+++ Data/main.java
@@ -0,0 +1,13 @@
+class main {
+	
+	public static void main( String[] args ){
+		
+		Data a = new Data();
+		Data b = new Data();
+		a.RecebeDados();  // metodo da classe Data para receber os dados
+		a.imprime(); // metodo da classe Data para impressao dos dados.
+		b.imprime(); // ira imprimir o que foi declarado como padrão.
+			
+		
+	}
+}
